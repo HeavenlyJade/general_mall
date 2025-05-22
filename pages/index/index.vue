0962962d61a185d4a8ae1e6968a4a063960b939a
XF <1131680660@qq.com>
2025-05-22 16:58:52 +0800

    modify:修改index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,31 @@
 <template>
 	<view class="page">
-		<!-- 顶部文字 -->
-
+		<!-- 顶部安全区域 -->
+		<view class="status-bar-height"></view>
+		
+		<!-- 顶部搜索栏 -->
+		<view class="search-header">
+			<view class="store-logo" v-if="logoUrl">
+				<image :src="logoUrl" mode="aspectFit" class="logo-image"></image>
+				<text v-if="logoName" class="logo-name">{{logoName}}</text>
+			</view>
+			<view class="store-name" v-else>金酱酒业</view>
+			<view class="search-box" @click="toSearch">
+				<input 
+					class="search-input" 
+					type="text" 
+					placeholder="输入关键词搜索" 
+					placeholder-class="placeholder-style"
+					confirm-type="search"
+					v-model="searchKeyword"
+					@confirm="handleSearch"
+				/>
+				<image src="/static/icons/search.png" mode="aspectFit" class="search-icon" @click.stop="handleSearch"></image>
+			</view>
+			<view class="header-icons">
+				<image src="/static/icons/menu.png" mode="aspectFit" class="menu-icon"></image>
+			</view>
+		</view>
 
 		<!-- 分享模块组件 -->
 		<share-module :title="shareTitle" :imageUrl="shareImage" :path="sharePath" :content="shareContent"
@@ -21,31 +45,56 @@
 			</swiper>
 		</view>
 
-		<!-- 产品类型轮播 -->
-		<view class="category-scroll">
-			<scroll-view scroll-x="true" class="category-scroll-view">
-				<view class="category-item" v-for="(item, index) in categoryItems" :key="index" @click="toCategory(item, index)">
-					<image :src="item.icon || item.image" mode="aspectFit" class="category-image" />
-					<text class="category-name">{{item.name}}</text>
+		<!-- 产品类型导航 -->
+		<view class="category-nav">
+			<view class="category-item" v-for="(item, index) in categoryItems" :key="index" @click="toCategory(item, index)">
+				<image :src="item.icon || item.image" mode="aspectFit" class="category-image" />
+				<text class="category-name">{{item.name}}</text>
+			</view>
+		</view>
+
+		<!-- 滚动公告栏 -->
+		<view class="notice-bar">
+			<image src="/static/icons/notice.png" class="notice-icon"></image>
+			<swiper class="notice-swiper" vertical autoplay circular interval="3000">
+				<swiper-item v-for="(item, index) in noticeList" :key="index">
+					<text class="notice-text">{{item.content}}</text>
+				</swiper-item>
+			</swiper>
+			<view class="notice-more">更多</view>
+		</view>
+
+		<!-- 爆款推荐 -->
+		<view class="hot-recommend">
+			<view class="section-title">
+				<image src="/static/icons/fire.png" class="title-icon"></image>
+				<text>爆款推荐</text>
+			</view>
+			
+			<view class="product-list">
+				<view class="product-card" v-for="(product, index) in hotProducts" :key="index" @click="todetail(product)">
+					<image :src="getFirstImage(product.images)" mode="aspectFill" class="product-image" />
+					<view class="product-info">
+						<view class="product-name">{{product.name}}</view>
+						<view class="product-desc">{{product.alias}}</view>
+						<view class="product-price">¥ {{product.price}}</view>
+					</view>
 				</view>
-			</scroll-view>
+			</view>
 		</view>
 
 		<!-- 品牌介绍和产品系列组合 -->
-		<view class="category-section" v-for="(item, index) in categoryList" :key="item.id">
-			<!-- 品牌介绍 -->
+		<!-- <view class="category-section" v-for="(item, index) in categoryList" :key="item.id">
 			<view class="brand-intro">
 				<view class="brand-content" :style="{ 'background-image': `url(${item.icon})` }">
 					<view class="content-overlay">
 						<view class="text-container">
-							<!-- <view class="title">{{item.name}}</view> -->
 							<rich-text :nodes="item.content"></rich-text>
 						</view>
 					</view>
 				</view>
 			</view>
 
-			<!-- 对应的产品系列 -->
 			<view class="product-grid">
 				<view class="product-item" v-for="product in productList[item.id]" :key="product.id"
 					@click="todetail(product)">
@@ -53,10 +102,11 @@
 					<view class="product-info">
 						<view class="name">{{ product.name }}</view>
 						<view class="sub-name">{{ product.alias }}</view>
+						<view class="price">¥ {{ product.price || '198' }}</view>
 					</view>
 				</view>
 			</view>
-		</view>
+		</view> -->
 	</view>
 </template>
 
@@ -98,15 +148,30 @@ export default {
 
 	data() {
 		return {
+			searchKeyword: '',
 			categoryList: [],
 			productList: {},
 			bannerImage: '',
+			logoUrl: '',
+			logoName: '',
+			
+			// 分类导航
+			categoryItems: [],
+			
+			// 爆款推荐产品
+			hotProducts: [],
+			
+			// 通知公告
+			noticeList: [
+				{ content: '最新促销活动：购买满888元送精美礼品' },
+				{ content: '新品上市：十大酱香味已上线' }
+			],
 
 			// 分享相关数据
-			shareTitle: '东箭酒业 - 传统美酒',
+			shareTitle: '金酱酒业 - 传统美酒',
 			shareImage: '',
 			sharePath: '/pages/index/index',
-			shareContent: '精选东箭传统美酒，品味非凡',
+			shareContent: '精选金酱传统美酒，品味非凡',
 
 			// 统计分析数据
 			pageViews: 0,
@@ -119,7 +184,10 @@ export default {
 	},
 	onLoad(options) {
 		this.loadBannerData()
+		this.loadLogoData()
+		this.loadCategoryItems()
 		this.loadCategoryData()
+		this.loadHotProducts()
 
 		// 处理场景值和统计
 		if (options.scene) {
@@ -137,10 +205,25 @@ export default {
 	},
 	onShow() {
 		this.loadBannerData()
+		this.loadLogoData()
+		this.loadCategoryItems()
 		this.loadCategoryData()
+		this.loadHotProducts()
 	},
 	methods: {
-
+		// 搜索相关方法
+		toSearch() {
+			uni.navigateTo({
+				url: `/pages/goods/HM-search`
+			})
+		},
+		
+		handleSearch() {
+			uni.navigateTo({
+				url: `/pages/goods/HM-search?keyword=${this.searchKeyword}`
+			})
+		},
+		
 		loadBannerData() {
 			this.$get('/wx_mini_app/banners/by-type/index_bg', {}, res => {
 				if (res.items) {
@@ -148,6 +231,35 @@ export default {
 					if (this.bannerList.length > 0) {
 						this.bannerImage = this.bannerList[0].upload_image || ''
 					}
+				}
+			})
+		},
+		
+		loadLogoData() {
+			this.$get('/wx_mini_app/banners/by-type/logo', {}, res => {
+				if (res.items && res.items.length > 0) {
+					this.logoUrl = res.items[0].upload_image || ''
+					this.logoName = res.items[0].name || ''
+				}
+			})
+		},
+		
+		loadCategoryItems() {
+			this.$get('/wx_mini_app/product-category', {
+				page: 1,
+				size: 100
+			}, res => {
+				if (res.code === 200 && res.data) {
+					// 获取所有分类并排序
+					const sortedCategories = res.data.sort((a, b) => a.sort_order - b.sort_order);
+					// 只取前4个分类用于导航
+					this.categoryItems = sortedCategories.slice(0, 4).map(item => {
+						return {
+							id: item.id,
+							name: item.name,
+							icon: item.icon || item.image
+						}
+					})
 				}
 			})
 		},
@@ -184,9 +296,20 @@ export default {
 				}
 			})
 		},
+		loadHotProducts() {
+			this.$get('/wx_mini_app/shop-product', {
+				page: 1,
+				size: 4,
+				need_total_count: true,
+				is_hot: 1
+			}, res => {
+				if (res.code === 200) {
+					this.hotProducts = res.data
+				}
+			})
+		},
 		getFirstImage(images) {
 			try {
-
 				return images[0] || ''
 			} catch (e) {
 				return ''
@@ -196,6 +319,9 @@ export default {
 			let id = item.id
 			let url = `/pages/goods/detail?id=${id}`
 			this.$navigateTo(url)
+		},
+		toCategory(item, index) {
+			this.$navigateTo(`/pages/category/list?id=${item.id || index}&name=${item.name}`)
 		},
 		toWholesale() {
 			this.$navigateTo('/pages/wholesale/index')
@@ -274,282 +400,326 @@ export default {
 
 <style lang="scss">
 .page {
-	background: #f5f5f5;
+	background: #f8f8f8;
 	min-height: 100vh;
 }
 
-.video-banner {
-	position: relative;
+/* 顶部安全区域 */
+.status-bar-height {
+	height: var(--status-bar-height);
 	width: 100%;
-	height: 400rpx;
-	background: #fff;
-
-	image {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
+	background-color: #fff;
 }
 
-.brand-intro {
-	margin: 20rpx;
-
-	.brand-content {
-		position: relative;
-		width: 100%;
-		height: 180rpx;
-		background-size: cover;
-		background-position: center;
-		background-repeat: no-repeat;
-		border-radius: 20rpx;
-		overflow: hidden;
-
-		.content-overlay {
-			position: absolute;
-			left: 0;
-			top: 0;
-			width: 100%;
-			height: 100%;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-
-			.text-container {
-				width: 70%;
-				padding: 20rpx 30rpx;
-				text-align: center;
-
-				:deep(rich-text) {
-					font-size: 24rpx;
-					color: #333;
-					line-height: 1.6;
-
-					div {
-						margin-bottom: 4rpx;
-						overflow: hidden;
-						text-overflow: ellipsis;
-						display: -webkit-box;
-						-webkit-line-clamp: 2;
-						-webkit-box-orient: vertical;
-
-						strong {
-							display: block;
-							font-size: 28rpx;
-							margin-bottom: 8rpx;
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-.product-grid {
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	gap: 20rpx;
-	padding: 20rpx;
-
-	.product-item {
-		position: relative;
-		height: 460rpx;
-		width: 320rpx;
-		margin: 0 auto;
-		background: #fff;
-		border-radius: 12rpx;
-		overflow: hidden;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		padding: 0;
-		box-shadow: none;
-		padding: 20rpx;
-		box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
-
-		image {
-			width: 100%;
-			height: 380rpx;
-			object-fit: contain;
-			margin-bottom: 0;
-		}
-
-		.product-info {
-			position: absolute;
-			width: 100%;
-			text-align: center;
-			padding: 10rpx;
-			bottom: 0;
-			background: rgba(255, 255, 255, 0.9);
-
-			.name {
-				font-size: 28rpx;
-				color: #8B0000;
-				font-weight: 600;
-				margin-bottom: 6rpx;
-			}
-
-			.sub-name {
-				font-size: 24rpx;
-				color: #996633;
-			}
-		}
-	}
-}
-
-.collection-series {
+/* 顶部搜索栏 */
+.search-header {
 	display: flex;
-	padding: 20rpx;
-	gap: 20rpx;
-
-	.collection-item {
-		flex: 1;
-		height: 500rpx;
-		position: relative;
-		border-radius: 12rpx;
-		overflow: hidden;
-
-		image {
-			width: 100%;
-			height: 100%;
-			object-fit: cover;
-		}
-
-		.collection-info {
-			position: absolute;
-			left: 20rpx;
-			top: 20rpx;
-			color: #0066cc;
-
-			.name {
-				font-size: 40rpx;
-				font-weight: bold;
-			}
-
-			.sub-name {
-				font-size: 28rpx;
-			}
-		}
-	}
+	align-items: center;
+	padding: 20rpx 30rpx;
+	background: #fff;
+	position: sticky;
+	top: var(--status-bar-height);
+	z-index: 100;
 }
 
+.store-logo {
+	height: 60rpx;
+	margin-right: 20rpx;
+	margin-left: -10rpx;
+	display: flex;
+	align-items: center;
+}
+
+.logo-image {
+	height: 100rpx;
+	width: 100rpx;
+	margin-right: 10rpx;
+	flex-shrink: 0;
+}
+
+.logo-name {
+	font-size: 32rpx;
+	color: #333;
+	font-weight: bold;
+}
+
+.store-name {
+	font-size: 32rpx;
+	font-weight: bold;
+	color: #333;
+	margin-right: 20rpx;
+}
+
+.search-box {
+	flex: 1;
+	height: 64rpx;
+	background: #f5f5f5;
+	border-radius: 32rpx;
+	display: flex;
+	align-items: center;
+	padding: 0 20rpx;
+}
+
+.search-input {
+	flex: 1;
+	height: 60rpx;
+	font-size: 28rpx;
+	color: #333;
+	background: transparent;
+}
+
+.placeholder-style {
+	color: #999;
+	font-size: 28rpx;
+	margin-left: 10rpx;
+}
+
+.search-icon {
+	width: 40rpx;
+	height: 40rpx;
+	margin-left: auto;
+}
+
+.header-icons {
+	margin-left: 20rpx;
+}
+
+.menu-icon {
+	width: 48rpx;
+	height: 48rpx;
+}
+
+/* Banner样式 */
 .banner-container {
 	width: 100%;
 	height: 400rpx;
 	position: relative;
-
-	.banner-swiper {
-		width: 100%;
-		height: 100%;
-	}
-
-	.safe-area-container {
-		width: 100%;
-		height: 100%;
-		padding: 60rpx 30rpx 20rpx 30rpx;
-		/* 增加顶部和两侧安全距离 */
-		box-sizing: border-box;
-	}
-
-	.banner-image,
-	.banner-video {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		border-radius: 12rpx;
-		/* 增加圆角效果 */
-	}
 }
 
-.wholesale-section {
-	padding: 40rpx 30rpx;
+.banner-swiper {
+	width: 100%;
+	height: 100%;
+}
+
+.banner-image,
+.banner-video {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+}
+
+/* 分类导航 */
+.category-nav {
+	display: flex;
+	justify-content: space-between;
 	background: #fff;
-
-	.title {
-		font-size: 36rpx;
-		text-align: center;
-		margin-bottom: 20rpx;
-	}
-
-	.desc {
-		font-size: 26rpx;
-		color: #666;
-		text-align: center;
-		margin-bottom: 30rpx;
-		line-height: 1.6;
-	}
-
-	.wholesale-banner {
-		position: relative;
-		height: 300rpx;
-		border-radius: 12rpx;
-		overflow: hidden;
-
-		image {
-			width: 100%;
-			height: 100%;
-			object-fit: cover;
-		}
-
-		.banner-text {
-			position: absolute;
-			left: 40rpx;
-			top: 40rpx;
-			color: #d81e06;
-			font-size: 40rpx;
-			font-weight: bold;
-		}
-
-		.sub-text {
-			position: absolute;
-			left: 40rpx;
-			top: 100rpx;
-			color: #333;
-			font-size: 28rpx;
-		}
-	}
-}
-
-.category-section {
-	margin-bottom: 40rpx;
-
-	.brand-intro {
-		margin: 20rpx;
-	}
-}
-
-.category-scroll {
-	background-color: #fff;
-	padding: 20rpx;
+	padding: 30rpx 20rpx;
 	margin: 20rpx;
 	border-radius: 16rpx;
-	box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
-
-	.category-scroll-view {
-		white-space: nowrap;
-		width: 100%;
-	}
-
-	.category-item {
-		display: inline-block;
-		width: 170rpx;
-		text-align: center;
-		padding: 20rpx 0;
-
-		.category-image {
-			width: 120rpx;
-			height: 120rpx;
-			object-fit: contain;
-		}
-
-		.category-name {
-			display: block;
-			font-size: 28rpx;
-			color: #333;
-			margin-top: 15rpx;
-			white-space: nowrap;
-			overflow: hidden;
-			text-overflow: ellipsis;
-		}
-	}
 }
+
+.category-item {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 25%;
+}
+
+.category-image {
+	width: 100rpx;
+	height: 100rpx;
+	margin-bottom: 15rpx;
+}
+
+.category-name {
+	font-size: 26rpx;
+	color: #333;
+}
+
+/* 公告栏 */
+.notice-bar {
+	display: flex;
+	align-items: center;
+	height: 70rpx;
+	background: #fff9f0;
+	margin: 0 20rpx 20rpx;
+	padding: 0 20rpx;
+	border-radius: 10rpx;
+}
+
+.notice-icon {
+	width: 36rpx;
+	height: 36rpx;
+	margin-right: 10rpx;
+}
+
+.notice-swiper {
+	flex: 1;
+	height: 100%;
+}
+
+.notice-text {
+	font-size: 24rpx;
+	color: #ff6633;
+	line-height: 70rpx;
+}
+
+.notice-more {
+	font-size: 24rpx;
+	color: #999;
+	margin-left: 20rpx;
+}
+
+/* 爆款推荐 */
+.hot-recommend {
+	background: #fff;
+	margin: 0 20rpx 20rpx;
+	padding: 20rpx;
+	border-radius: 16rpx;
+}
+
+.section-title {
+	display: flex;
+	align-items: center;
+	margin-bottom: 20rpx;
+}
+
+.title-icon {
+	width: 40rpx;
+	height: 40rpx;
+	margin-right: 10rpx;
+}
+
+.section-title text {
+	font-size: 30rpx;
+	font-weight: bold;
+	color: #333;
+}
+
+.product-list {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+}
+
+.product-card {
+	width: 48%;
+	margin-bottom: 20rpx;
+	background: #fff;
+	border-radius: 12rpx;
+	overflow: hidden;
+	box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
+}
+
+.product-image {
+	width: 100%;
+	height: 320rpx;
+	object-fit: cover;
+}
+
+.product-info {
+	padding: 15rpx;
+}
+
+.product-name {
+	font-size: 28rpx;
+	color: #333;
+	font-weight: bold;
+}
+
+.product-desc {
+	font-size: 24rpx;
+	color: #666;
+	margin-top: 5rpx;
+}
+
+.product-price {
+	font-size: 30rpx;
+	color: #e60012;
+	font-weight: bold;
+	margin-top: 10rpx;
+}
+
+/* 品牌介绍 */
+.brand-intro {
+	margin: 20rpx;
+}
+
+.brand-content {
+	position: relative;
+	width: 100%;
+	height: 180rpx;
+	background-size: cover;
+	background-position: center;
+	border-radius: 16rpx;
+	overflow: hidden;
+}
+
+.content-overlay {
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.text-container {
+	width: 70%;
+	padding: 20rpx;
+	text-align: center;
+}
+
+:deep(rich-text) {
+	font-size: 24rpx;
+	color: #333;
+	line-height: 1.6;
+}
+
+/* 产品网格 */
+.product-grid {
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	gap: 20rpx;
+	padding: 0 20rpx 20rpx;
+}
+
+.product-item {
+	background: #fff;
+	border-radius: 12rpx;
+	overflow: hidden;
+	box-shadow: 0 2rpx 10rpx rgba(0, 0, 0, 0.05);
+}
+
+.product-item image {
+	width: 100%;
+	height: 320rpx;
+	object-fit: cover;
+}
+
+.product-item .product-info {
+	padding: 15rpx;
+}
+
+.product-item .name {
+	font-size: 28rpx;
+	color: #333;
+	font-weight: bold;
+}
+
+.product-item .sub-name {
+	font-size: 24rpx;
+	color: #666;
+	margin-top: 5rpx;
+}
+
+.product-item .price {
+	font-size: 30rpx;
+	color: #e60012;
+	font-weight: bold;
+	margin-top: 10rpx;
+}
+
 </style>
